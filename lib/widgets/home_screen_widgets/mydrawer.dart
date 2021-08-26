5c40d6c8ce757a9c20e804645faaca8d65931071
apoorvpandey0 <apoorvpandey0@gmail.com>
2021-08-27 02:28:05 +0530

    code refactoring

--- lib/widgets/home_screen_widgets/mydrawer.dart
+++ lib/widgets/home_screen_widgets/mydrawer.dart
@@ -5,12 +5,33 @@ import 'package:url_launcher/url_launcher.dart';
 
 // import 'package:fluttertoast/fluttertoast.dart';
 
-class MyDrawer extends StatefulWidget {
+class MyDrawer extends StatelessWidget {
   @override
-  _MyDrawerState createState() => _MyDrawerState();
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.70,
+      child: ClipRRect(
+        child: Drawer(
+          child: Column(
+            children: <Widget>[
+              MyDrawerHeader(),
+              // Internships and settings tab section
+              DrawerOptionsColumn()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class _MyDrawerState extends State<MyDrawer> {
+class DrawerOptionsColumn extends StatefulWidget {
+  @override
+  _DrawerOptionsColumnState createState() => _DrawerOptionsColumnState();
+}
+
+class _DrawerOptionsColumnState extends State<DrawerOptionsColumn> {
+  @override
   int _selectedTile = -1;
 
   void _setSelectedTile(int tile) {
@@ -19,155 +40,133 @@ class _MyDrawerState extends State<MyDrawer> {
     });
   }
 
-  @override
   Widget build(BuildContext context) {
-    final auth = Provider.of<AuthProvider>(context);
-    return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.70,
-      child: ClipRRect(
-        // borderRadius: BorderRadius.only(bottomRight: Radius.circular(20)),
-        child: Drawer(
-          child: Column(
-            // padding: EdgeInsets.zero,
-            children: <Widget>[
-              // Container(
-              // height: MediaQuery.of(context).padding.top,
-              // ),
-              // Container lets us decide the height of the Header Box
-              Container(
-                  height: 200,
-                  child: DrawerHeader(
-                    padding: EdgeInsets.zero,
-                    margin: EdgeInsets.zero,
-                    child: Stack(children: [
-                      Container(
-                        height: 200,
-                        child: Image.network(
-                          'https://image.freepik.com/free-vector/abstract-wallpaper_23-2148663179.jpg',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          CircleAvatar(
-                              radius: 45,
-                              backgroundImage: NetworkImage(
-                                  'https://randomuser.me/api/portraits/lego/0.jpg')),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              FittedBox(
-                                child: Text(
-                                  auth.user.username,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                              FittedBox(
-                                child: Text(
-                                  'apoorvpandey0@gmail.com',
-                                  style: TextStyle(
-                                      fontSize: 10, color: Colors.white70),
-                                ),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                    ]),
-                  )),
-              // Spacer(),
-              // Internships and settings tab section
-              Card(
-                color: _selectedTile == 0 ? Colors.white : Colors.white10,
-                elevation: _selectedTile == 0 ? 10 : 0,
-                child: ListTile(
-                  leading: Icon(
-                    Icons.book,
-                    size: 26,
-                  ),
-                  title: Text('Notes',
-                      style: TextStyle(
-                        fontSize: 20,
-                      )),
-                  onTap: () {
-                    _setSelectedTile(0);
-                    // Navigator.of(context)
-                    //     .pushReplacementNamed(TabsScreen.routeName);
-                  },
-                  selected: _selectedTile == 0 ? true : false,
-                ),
-              ),
-              Card(
-                color: _selectedTile == 1 ? Colors.white : Colors.white10,
-                elevation: _selectedTile == 1 ? 10 : 0,
-                child: ListTile(
-                  leading: Icon(
-                    Icons.filter,
-                    size: 26,
-                  ),
-                  title: Text('Syllabus',
-                      style: TextStyle(
-                        fontSize: 20,
-                      )),
-                  onTap: () {
-                    _setSelectedTile(1);
+    return Column(
+      children: [
+        ListTile(
+          leading: Icon(
+            Icons.book,
+            size: 26,
+          ),
+          title: Text('Notes',
+              style: TextStyle(
+                fontSize: 20,
+              )),
+          onTap: () {
+            _setSelectedTile(0);
+            // Navigator.of(context)
+            //     .pushReplacementNamed(TabsScreen.routeName);
+          },
+          selected: _selectedTile == 0 ? true : false,
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.library_books_outlined,
+            size: 26,
+          ),
+          title: Text('Syllabus',
+              style: TextStyle(
+                fontSize: 20,
+              )),
+          onTap: () {
+            _setSelectedTile(1);
 
-                    // Navigator.of(context)
-                    //     .pushReplacementNamed(FiltersScreen.routeName);
-                  },
-                  selected: _selectedTile == 1 ? true : false,
-                ),
-              ),
-              Card(
-                color: _selectedTile == 2 ? Colors.white : Colors.white10,
-                elevation: _selectedTile == 2 ? 10 : 0,
-                child: ListTile(
-                  leading: Icon(
-                    Icons.wallet_giftcard,
-                    size: 26,
-                  ),
-                  title: Text('Refer and earn',
-                      style: TextStyle(
-                        fontSize: 20,
-                      )),
-                  onTap: () {
-                    _setSelectedTile(2);
+            // Navigator.of(context)
+            //     .pushReplacementNamed(FiltersScreen.routeName);
+          },
+          selected: _selectedTile == 1 ? true : false,
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.wallet_giftcard,
+            size: 26,
+          ),
+          title: Text('Refer and earn',
+              style: TextStyle(
+                fontSize: 20,
+              )),
+          onTap: () {
+            _setSelectedTile(2);
 
-                    // Navigator.of(context)
-                    //     .pushReplacementNamed(FiltersScreen.routeName);
-                  },
-                  selected: _selectedTile == 2 ? true : false,
-                ),
-              ),
-              Card(
-                color: _selectedTile == 3 ? Colors.white : Colors.white10,
-                elevation: _selectedTile == 3 ? 10 : 0,
-                child: ListTile(
-                  leading: Icon(Icons.contact_support),
-                  title: Text('Contact Us',
-                      style: TextStyle(
-                        fontSize: 19,
-                      )),
-                  onTap: () {
-                    _setSelectedTile(3);
-                  },
-                  selected: _selectedTile == 3 ? true : false,
-                  subtitle: Text(
-                    'Mon-Fri,10 AM-5PM',
-                    style: TextStyle(fontSize: 13),
-                  ),
-                ),
-              ),
-            ],
+            // Navigator.of(context)
+            //     .pushReplacementNamed(FiltersScreen.routeName);
+          },
+          selected: _selectedTile == 2 ? true : false,
+        ),
+        ListTile(
+          leading: Icon(Icons.contact_support),
+          title: Text('Contact Us',
+              style: TextStyle(
+                fontSize: 19,
+              )),
+          onTap: () {
+            _setSelectedTile(3);
+          },
+          selected: _selectedTile == 3 ? true : false,
+          subtitle: Text(
+            'Mon-Fri,10 AM-5PM',
+            style: TextStyle(fontSize: 13),
           ),
         ),
-      ),
+      ],
+    );
+  }
+}
+
+class MyDrawerHeader extends StatelessWidget {
+  const MyDrawerHeader({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AuthProvider>(
+      builder: (ctx, value, child) => Container(
+          height: 200,
+          child: DrawerHeader(
+            padding: EdgeInsets.zero,
+            margin: EdgeInsets.zero,
+            child: Stack(children: [
+              Container(
+                height: 200,
+                child: Image.network(
+                  'https://image.freepik.com/free-vector/abstract-wallpaper_23-2148663179.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                      radius: 45,
+                      backgroundImage: NetworkImage(
+                          'https://randomuser.me/api/portraits/lego/0.jpg')),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FittedBox(
+                        child: Text(
+                          value.user.username,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      FittedBox(
+                        child: Text(
+                          value.user.phone,
+                          style: TextStyle(fontSize: 10, color: Colors.white70),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ]),
+          )),
     );
   }
 }
