95c4e4e77876de1d314d56560fd0d0ae97b43dc3
apoorvpandey0 <apoorvpandey0@gmail.com>
2021-08-27 02:03:00 +0530

    implemmented fetch and show papers

--- lib/providers/papers.dart
+++ lib/providers/papers.dart
@@ -1,25 +1,42 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:quiz_app/utils/api_constants.dart';
 
-class Paper {
-  final String title;
-  final String imageUrl;
-  Paper({this.title, this.imageUrl});
+class PaperModel {
+  String id;
+  String title;
+  String imageUrl;
+  PaperModel({this.id, this.title, this.imageUrl});
+  //Constructor to create object from json
+  PaperModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'].toString();
+    title = json['name'];
+    imageUrl = json['imageUrl'];
+  }
 }
 
-class Papers with ChangeNotifier {
-  List<Paper> _papers = [];
+class PapersProvider with ChangeNotifier {
+  List<PaperModel> _papers = [];
+  get papers => _papers;
 
   String _authToken;
-  void getAndSetPapers() async {
+  Future getAndSetPapers() async {
+    _papers.clear();
     final Uri url = Uri.parse(APIConstants.GET_PAPERS_URL);
     final response = await http.get(url);
     print(response.body);
+    // create paper objects from response.body
+    final extData = json.decode(response.body);
+    for (Map<String, dynamic> item in extData) {
+      _papers.add(PaperModel.fromJson(item));
+    }
+    return _papers;
   }
 
   void updateProxyMethod(
-      String token, List<Paper> prevOrders, String userName) {
+      String token, List<PaperModel> prevOrders, String userName) {
     _papers = prevOrders;
     // userName = userName;
     _authToken = token;
