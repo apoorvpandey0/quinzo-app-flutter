fc33ec82fd178f19be773a6743556f597950f757
apoorvpandey0 <apoorvpandey0@gmail.com>
2021-08-27 02:15:46 +0530

    code refactoring and comments

--- lib/widgets/home_screen_widgets/profile_page.dart
+++ lib/widgets/home_screen_widgets/profile_page.dart
@@ -7,21 +7,9 @@ import 'package:quiz_app/providers/papers.dart';
 import 'package:quiz_app/providers/settings.dart';
 // import 'package:audioplayers/audio_cache.dart';
 
-class ProfilePage extends StatefulWidget {
-  @override
-  _ProfilePageState createState() => _ProfilePageState();
-}
-
-class _ProfilePageState extends State<ProfilePage> {
-  bool quizReminders = true;
-  bool darkMode;
-  // final player = AudioCache();
-
+class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final settingsData = Provider.of<Settings>(context);
-    darkMode = settingsData.darkMode;
-    final auth = Provider.of<AuthProvider>(context);
     return Scaffold(
         body: Padding(
       padding: const EdgeInsets.all(8.0),
@@ -29,199 +17,207 @@ class _ProfilePageState extends State<ProfilePage> {
           height: MediaQuery.of(context).size.height,
           child: Column(
             children: [
-              Card(
-                color: Colors.amber,
-                elevation: 10,
-                // color: Colors.deepPurple,
-                child: Stack(children: [
-                  Container(
-                    height: 120,
-                    width: MediaQuery.of(context).size.width,
-                    child: Image.network(
-                      'https://image.freepik.com/free-vector/abstract-wallpaper_23-2148663179.jpg',
-                      fit: BoxFit.fitWidth,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(100),
-                          child: FadeInImage.assetNetwork(
-                              height: 100,
-                              placeholder: 'assets/images/placeholder.jpeg',
-                              image:
-                                  'https://randomuser.me/api/portraits/lego/0.jpg'),
-                        ),
-                        VerticalDivider(),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              auth.user.username,
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            Text(
-                              '+917898812907',
-                              style: TextStyle(
-                                  fontSize: 12, color: Colors.white70),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ]),
-              ),
+              UserCardWidget(),
               SizedBox(
                 height: 8,
               ),
-              Card(
-                child: Column(
-                  children: [
-                    ListTile(
-                      leading: Icon(Icons.notifications),
-                      title: Text('Enable Reminders'),
-                      trailing: CupertinoSwitch(
-                          // activeColor: Colors.amber,
-                          value: quizReminders,
-                          onChanged: (ctx) {
-                            setState(() {
-                              quizReminders = !quizReminders;
-                            });
-                            // player.play('assets/sounds/switch.mp3');
-                            // SystemSound.play(SystemSoundType.click);
-                            settingsData.toggleReminders();
-                          }),
-                    ),
-                    Divider(
-                      height: 5,
-                      color: Colors.black26,
-                      indent: 30,
-                      endIndent: 30,
-                    ),
-                    // ListTile(
-                    //   leading: Icon(Icons.notifications),
-                    //   title: Text('Enable Reminders'),
-                    //   trailing:
-                    //       CupertinoSwitch(value: true, onChanged: (ctx) {}),
-                    // ),
-                    // Divider(
-                    //   height: 5,
-                    //   color: Colors.black26,
-                    //   indent: 30,
-                    //   endIndent: 30,
-                    // ),
-                    ListTile(
-                      leading: Icon(Icons.settings),
-                      title: Text('Dark Mode'),
-                      trailing: CupertinoSwitch(
-                          value: darkMode,
-                          onChanged: (ctx) {
-                            setState(() {
-                              darkMode = !darkMode;
-                            });
-                            settingsData.toggleAppMode();
-                          }),
-                    ),
-                    Divider(
-                      height: 5,
-                      color: Colors.black26,
-                      indent: 30,
-                      endIndent: 30,
-                    ),
-                    ListTile(
-                        leading: Icon(Icons.policy_outlined),
-                        title: Text('Privacy policy'),
-                        // onTap: () {
-                        //   showBottomSheet(
-                        //       context: context,
-                        //       builder: (ctx) => Padding(
-                        //             padding: const EdgeInsets.all(10),
-                        //             child: Text(
-                        //                 "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
-                        //           ));
-                        // },
-                        trailing: Icon(Icons.navigate_next)
-                        // trailing: CupertinoSwitch(value: true, onChanged: (ctx) {}),
-                        ),
-                    Divider(
-                      height: 5,
-                      color: Colors.black26,
-                      indent: 30,
-                      endIndent: 30,
-                    ),
-                    ListTile(
-                      onTap: () async {
-                        List<PaperModel> papers =
-                            await Provider.of<PapersProvider>(context,
-                                    listen: false)
-                                .getAndSetPapers();
-                        showDialog(
-                            context: context,
-                            builder: (_) => AlertDialog(
-                                  title: Text("Select paper:"),
-                                  content: Container(
-                                      height: 300,
-                                      width: 300,
-                                      child: ListView.builder(
-                                        itemCount: papers.length,
-                                        itemBuilder: (context, index) =>
-                                            ListTile(
-                                          onTap: () {
-                                            //           await Provider.of<PapersProvider>(context,
-                                            // listen: false).
-                                          },
-                                          title: Text(papers[index].title),
-                                        ),
-                                      )),
-                                ));
-                      },
-                      leading: Icon(Icons.star_border),
-                      title: Text('SSC CGL'),
-                      trailing: Icon(Icons.navigate_next),
-                    ),
-                    Divider(
-                      height: 5,
-                      color: Colors.black26,
-                      indent: 30,
-                      endIndent: 30,
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.logout),
-                      title: Text('Logout'),
-                      onTap: () async {
-                        await showDialog(
-                            context: context,
-                            builder: (context) => AlertDialog(
-                                  content:
-                                      Text('Do you really wish to log out?'),
-                                  actions: [
-                                    FlatButton(
-                                        onPressed: () {
-                                          if (settingsData.darkMode)
-                                            settingsData.toggleAppMode();
-                                          auth.logout();
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: Text('Yeah')),
-                                    FlatButton(
-                                        onPressed: () {
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: Text('Nopes'))
-                                  ],
-                                ));
-                      },
-                      // trailing: CupertinoSwitch(value: true, onChanged: (ctx) {}),
-                    )
-                  ],
-                ),
-              ),
+              ProfileOptionsListWidget(),
             ],
           )),
     ));
+  }
+}
+
+class ProfileOptionsListWidget extends StatefulWidget {
+  @override
+  _ProfileOptionsListWidgetState createState() =>
+      _ProfileOptionsListWidgetState();
+}
+
+class _ProfileOptionsListWidgetState extends State<ProfileOptionsListWidget> {
+  bool quizReminders = true;
+
+  bool darkMode;
+
+  @override
+  Widget build(BuildContext context) {
+    final settingsData = Provider.of<Settings>(context);
+    final auth = Provider.of<AuthProvider>(context);
+    darkMode = settingsData.darkMode;
+    return Card(
+      child: Column(
+        children: [
+//------- Enable reminders button
+          ListTile(
+            leading: Icon(Icons.notifications),
+            title: Text('Enable Reminders'),
+            trailing: CupertinoSwitch(
+                // activeColor: Colors.amber,
+                value: quizReminders,
+                onChanged: (ctx) {
+                  setState(() {
+                    quizReminders = !quizReminders;
+                  });
+                  settingsData.toggleReminders();
+                }),
+          ),
+          Divider(
+            height: 5,
+            color: Colors.black26,
+            indent: 30,
+            endIndent: 30,
+          ),
+
+//------- Dark mode button
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Dark Mode'),
+            trailing: CupertinoSwitch(
+                value: darkMode,
+                onChanged: (ctx) {
+                  setState(() {
+                    darkMode = !darkMode;
+                  });
+                  settingsData.toggleAppMode();
+                }),
+          ),
+          Divider(
+            height: 5,
+            color: Colors.black26,
+            indent: 30,
+            endIndent: 30,
+          ),
+
+//------- Privacy policy button
+          ListTile(
+              leading: Icon(Icons.policy_outlined),
+              title: Text('Privacy policy'),
+              onTap: () {},
+              trailing: Icon(Icons.navigate_next)),
+          Divider(
+            height: 5,
+            color: Colors.black26,
+            indent: 30,
+            endIndent: 30,
+          ),
+
+//------- Select papers button
+          ListTile(
+            onTap: () async {
+              List<PaperModel> papers =
+                  await Provider.of<PapersProvider>(context, listen: false)
+                      .getAndSetPapers();
+              showDialog(
+                  context: context,
+                  builder: (_) => AlertDialog(
+                        title: Text("Select paper:"),
+                        content: Container(
+                            height: 300,
+                            width: 300,
+                            child: ListView.builder(
+                              itemCount: papers.length,
+                              itemBuilder: (context, index) => ListTile(
+                                onTap: () {
+                                  //           await Provider.of<PapersProvider>(context,
+                                  // listen: false).
+                                },
+                                title: Text(papers[index].title),
+                              ),
+                            )),
+                      ));
+            },
+            leading: Icon(Icons.star_border),
+            title: Text('SSC CGL'),
+            trailing: Icon(Icons.navigate_next),
+          ),
+          Divider(
+            height: 5,
+            color: Colors.black26,
+            indent: 30,
+            endIndent: 30,
+          ),
+
+//------- Logout button
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('Logout'),
+            onTap: () async {
+              await showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                        content: Text('Do you really wish to log out?'),
+                        actions: [
+                          FlatButton(
+                              onPressed: () {
+                                if (settingsData.darkMode)
+                                  settingsData.toggleAppMode();
+                                auth.logout();
+                                Navigator.of(context).pop();
+                              },
+                              child: Text('Yeah')),
+                          FlatButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Text('Nopes'))
+                        ],
+                      ));
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class UserCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AuthProvider>(
+      builder: (context, value, child) => Card(
+        // color: Colors.amber,
+        elevation: 10,
+        // color: Colors.deepPurple,
+        child: Stack(children: [
+          Container(
+            height: 120,
+            width: MediaQuery.of(context).size.width,
+            child: Image.network(
+              'https://image.freepik.com/free-vector/abstract-wallpaper_23-2148663179.jpg',
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: FadeInImage.assetNetwork(
+                      height: 100,
+                      placeholder: 'assets/images/placeholder.jpeg',
+                      image: 'https://randomuser.me/api/portraits/lego/0.jpg'),
+                ),
+                VerticalDivider(),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      value.user.username ?? "Not found",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Text(
+                      value.user.phone ?? "Not found",
+                      style: TextStyle(fontSize: 12, color: Colors.white70),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
